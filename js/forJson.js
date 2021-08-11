9faf097eefd185fbad1adb90c7834ba4da38c07c
Stanislav Aristarin <stanislav.aristarin@gmail.com>
2021-08-11 12:56:05 +0300

    returned the button for user

--- js/forJson.js
+++ js/forJson.js
@@ -189,17 +189,17 @@ window.addEventListener('load', function() {
 
 
 
-    /*function download(content, fileName, contentType) {
+    function download(content, fileName, contentType) {
         var a = document.createElement("a");
         var file = new Blob([JSON.stringify(content)], {type: contentType});
         a.href = URL.createObjectURL(file);
         a.download = fileName;
         a.click();
-    }*/
-    /*document.querySelector('.saveToFile').addEventListener('click', e => {
-        //download(dataForTable, 'result.json', 'text/plain'); 
-        editJSON();           
-    });*/
+    }
+    document.querySelector('.saveToFile').addEventListener('click', e => {
+        download(dataForTable, 'result.json', 'text/plain'); 
+        //editJSON();           
+    });
 
     setChangeListener(tabl, function(event){
 
